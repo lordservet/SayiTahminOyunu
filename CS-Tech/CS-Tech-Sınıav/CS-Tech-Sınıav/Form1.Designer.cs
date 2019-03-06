69fe4cc02646ba946dae5270b3151d9f40a9fdc5
lordservet <rdservetoksuz@gmail.com>
2019-03-06 23:53:15 +0300

    Add files via upload

---
+++ CS-Tech/CS-Tech-Sınıav/CS-Tech-Sınıav/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace CS_Tech_Sınıav
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tahminT = new System.Windows.Forms.TextBox();
+            this.tahminB = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.bilTahminT = new System.Windows.Forms.Label();
+            this.bilTahimB = new System.Windows.Forms.Button();
+            this.kulTahminSonucT = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.artiT = new System.Windows.Forms.TextBox();
+            this.degerB = new System.Windows.Forms.Button();
+            this.eksiT = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Aklınızdan 4 basamaklı bir sayı tutun.\r\n2. Tuttuğunuz sayıyı söylemeyin.\r\n3. B" +
+    "ilgisayarın tutuğu sayısı tahmin etmeye çalışın.\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tahminiz : ";
+            // 
+            // tahminT
+            // 
+            this.tahminT.Location = new System.Drawing.Point(101, 82);
+            this.tahminT.MaxLength = 4;
+            this.tahminT.Name = "tahminT";
+            this.tahminT.Size = new System.Drawing.Size(69, 20);
+            this.tahminT.TabIndex = 2;
+            // 
+            // tahminB
+            // 
+            this.tahminB.Location = new System.Drawing.Point(200, 79);
+            this.tahminB.Name = "tahminB";
+            this.tahminB.Size = new System.Drawing.Size(75, 23);
+            this.tahminB.TabIndex = 3;
+            this.tahminB.Text = "Tahmin Et";
+            this.tahminB.UseVisualStyleBackColor = true;
+            this.tahminB.Click += new System.EventHandler(this.tahminB_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tahmin Sonuçlarınız : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Bilgisayarın Tahmini : ";
+            // 
+            // bilTahminT
+            // 
+            this.bilTahminT.AutoSize = true;
+            this.bilTahminT.Location = new System.Drawing.Point(151, 231);
+            this.bilTahminT.Name = "bilTahminT";
+            this.bilTahminT.Size = new System.Drawing.Size(0, 13);
+            this.bilTahminT.TabIndex = 6;
+            // 
+            // bilTahimB
+            // 
+            this.bilTahimB.Location = new System.Drawing.Point(40, 205);
+            this.bilTahimB.Name = "bilTahimB";
+            this.bilTahimB.Size = new System.Drawing.Size(130, 23);
+            this.bilTahimB.TabIndex = 7;
+            this.bilTahimB.Text = "Bilgisyar Tahmin Etsin";
+            this.bilTahimB.UseVisualStyleBackColor = true;
+            this.bilTahimB.Click += new System.EventHandler(this.bilTahimB_Click);
+            // 
+            // kulTahminSonucT
+            // 
+            this.kulTahminSonucT.AutoSize = true;
+            this.kulTahminSonucT.Location = new System.Drawing.Point(151, 121);
+            this.kulTahminSonucT.Name = "kulTahminSonucT";
+            this.kulTahminSonucT.Size = new System.Drawing.Size(0, 13);
+            this.kulTahminSonucT.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(158, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Yeri doğru bilinen rakam adedi : ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 277);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(157, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Yeri yanlış bilinen rakam adedi : ";
+            // 
+            // artiT
+            // 
+            this.artiT.Location = new System.Drawing.Point(200, 250);
+            this.artiT.MaxLength = 1;
+            this.artiT.Name = "artiT";
+            this.artiT.Size = new System.Drawing.Size(29, 20);
+            this.artiT.TabIndex = 11;
+            this.artiT.Text = "0";
+            this.artiT.TextChanged += new System.EventHandler(this.artiT_TextChanged);
+            this.artiT.Enter += new System.EventHandler(this.artiT_Enter);
+            // 
+            // degerB
+            // 
+            this.degerB.Location = new System.Drawing.Point(200, 302);
+            this.degerB.Name = "degerB";
+            this.degerB.Size = new System.Drawing.Size(75, 23);
+            this.degerB.TabIndex = 12;
+            this.degerB.Text = "Değerlendir";
+            this.degerB.UseVisualStyleBackColor = true;
+            this.degerB.Click += new System.EventHandler(this.degerB_Click);
+            // 
+            // eksiT
+            // 
+            this.eksiT.Location = new System.Drawing.Point(200, 276);
+            this.eksiT.Name = "eksiT";
+            this.eksiT.Size = new System.Drawing.Size(29, 20);
+            this.eksiT.TabIndex = 13;
+            this.eksiT.Text = "0";
+            this.eksiT.TextChanged += new System.EventHandler(this.eksiT_TextChanged);
+            this.eksiT.Enter += new System.EventHandler(this.eksiT_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(624, 354);
+            this.Controls.Add(this.eksiT);
+            this.Controls.Add(this.degerB);
+            this.Controls.Add(this.artiT);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.kulTahminSonucT);
+            this.Controls.Add(this.bilTahimB);
+            this.Controls.Add(this.bilTahminT);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tahminB);
+            this.Controls.Add(this.tahminT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tahminT;
+        private System.Windows.Forms.Button tahminB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label bilTahminT;
+        private System.Windows.Forms.Button bilTahimB;
+        private System.Windows.Forms.Label kulTahminSonucT;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox artiT;
+        private System.Windows.Forms.Button degerB;
+        private System.Windows.Forms.TextBox eksiT;
+    }
+}
+
---
+++ CS-Tech/CS-Tech-Sınıav/CS-Tech-Sınıav/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CS_Tech_Sınıav
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //değişkenler
+        private int bilSayi, bilSonTahmin;
+        private int[,] bilTahminX = new int[4, 3];// *,0 son 
+        private int[,] bilTahminDeger=new int[2,2]; // 0 - arti || 1 - eksi
+        private bool ilkTahmin = false;
+        private bool siraKimde = true; //false - Kullanıcı //true - bilgisayar
+
+        //Değerlendirme Buton Kontrol
+        private void siraKontrol( int i)
+        {
+            if (i==0)
+            {
+                bilTahimB.Enabled = false;
+                tahminB.Enabled = true;
+            }
+            else if(i==1)
+            {
+                bilTahimB.Enabled = true;
+                tahminB.Enabled = false;
+            }
+            else if (i == 2)
+            {
+
+                bilTahimB.Enabled = false;
+                tahminB.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //4 basamaklı rastgele sayı
+            Random r = new Random();
+            bilSayi = r.Next(1000, 10000);
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    bilTahminX[i, j] = -1;
+                }
+            }
+
+            this.siraKontrol(0);
+        }
+
+        //rakam bazında karşılaştırma için sayının rakamlarını diziye aktarıyor
+        private int[] rakamla(int sayi)
+        {
+            int[] rakam = new int[4];
+            string s = sayi.ToString();
+            for (int i = 0; i < 4; i++)
+            {
+                rakam[i] = int.Parse(s[i].ToString());
+            }
+            return rakam;
+        }
+
+        //int dizisini sayıya dönüştür
+        private int sayila(int[] rakam)
+        {
+            string t = "";
+            foreach (int i in rakam)
+            {
+                t += i.ToString();
+            }
+            return int.Parse(t);
+        }
+
+        //kullanıcı tahmin kontrol
+        private void tahmin(int sayi)
+        {
+            int[] s1 = rakamla(bilSayi),s2 = rakamla(sayi);
+            int arti = 0, eksi = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (s1[i] == s2[j])
+                    {
+                        if (i == j) arti++;
+                        else eksi++;
+                        break;
+                    }
+                }
+            }
+            //kullanıcı tahmin sonucu yazdırma
+            string sonuc = "";
+            if (arti == 4)
+            {
+                sonuc = "Tebrikler Doğru tahmiin ettiniz\n Sayı : " + sayi;
+                kulTahminSonucT.Text = sonuc;
+                MessageBox.Show(sonuc);
+            }
+            else
+            {
+                sonuc = "";
+                if (arti != 0) sonuc += "+" + arti + "\n";
+                if (eksi != 0) sonuc += "-" + eksi + "\n";
+                sonuc += arti + " rakamın yerini doğru tahmin ettiniz.\n";
+                sonuc += eksi + " rakamı doğru tahmin ettiniz ama yerleri yanlış.";
+                if (arti == 0 && eksi == 0) sonuc = "Tekrar tahmin Etmeyi Deneyiniz";
+                kulTahminSonucT.Text = sonuc;
+            }
+
+        }
+
+        private void tahminB_Click(object sender, EventArgs e)
+        {
+            this.siraKontrol(1);
+            int sayi;
+            // kontrol başlangıç
+            if (tahminT.Text.Length != 4) MessageBox.Show("Lütfen 4 basamaklı bir sayı giriniz!");
+            else if (!int.TryParse(tahminT.Text, out sayi)) MessageBox.Show("Lütfen sadece rakam giriniz!");
+            else this.tahmin(sayi);
+            //kontrol bitiş
+        }
+
+
+        //bilgisayar tahmin kontrol -- Tahmin p ucu olduğu zman buraya gelir
+        private void bilTahmin()
+        {
+            int[] sonTah = rakamla(bilSonTahmin);
+            for (int i = 0; i < 4; i++)
+            {
+                bilTahminX[i, 0] = sonTah[i];
+            }
+            // artıda artış var ise tek rakam üzerinde işlem yaparak artışa sebep olduğu düşünülen rakam hafızaya alınır.
+            if (bilTahminDeger[0, 0] > bilTahminDeger[1, 0])
+            {
+                int i = 0;
+                //hafıza boş yer var mı diye kontrol edilir. kontrolde dolu bulunan yerler boşaltılır.
+                for (; i < 4; i++)
+                {
+                    if (bilTahminX[i, 1] == -1 && bilTahminX[i, 2] == -1) break;
+                    else bilTahminX[i, 1] = -1;
+
+                }
+                if (i != 4)
+                {
+                    bilTahminX[i, 1] = bilTahminX[i, 0];
+                    while (true) //hafızaya alınan rakamın yerine sonTahmin de bulunmayan bir rakam atanır.
+                    {
+                        int j = 0;
+                        for (; j < 4; j++)
+                        {
+                            if (i == 0 && bilTahminX[i, 1] == 9)
+                            {
+                                if (i != j && ((bilTahminX[i, 1] + 2) % 10) == bilTahminX[j, 0]) break;
+                            }
+                            else
+                            {
+                                if (i != j && ((bilTahminX[i, 1] + 1) % 10) == bilTahminX[j, 0]) break;
+                            }
+                        }
+                        if (j == 4)
+                        {
+                            bilTahminX[i, 0] = (bilTahminX[i, 0] + 1) % 10;
+                            break;
+                        }
+                    }
+                }
+            }
+            //artıda azalma olduysa hafızaya alınan son rakam doğrudur ve kalıcı hafızaya alınır.
+            else if (bilTahminDeger[0, 0] < bilTahminDeger[1, 0])
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (bilTahminX[i, 1] != -1)
+                    {
+                        bilTahminX[i, 2] = bilTahminX[i, 1];
+                        break;
+                    }
+                }
+            }
+            else//eğer artılar da değişme yok ise eksileri kontrol et
+            {
+                int j = -1;
+
+                if (bilTahminDeger[0, 1] > bilTahminDeger[1, 1])//eksilerde artış varsa
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (bilTahminX[i, 2] == -1 && bilTahminX[i, 2] == -1)
+                        {
+                            if (j != -1)
+                            {
+                                bilTahminX[i, 1] = bilTahminX[j, 0];
+                                bilTahminX[j, 1] = bilTahminX[i, 0];
+                                break;
+                            }
+                            else j = i;
+                        }
+                    }
+                }
+
+                else if (bilTahminDeger[0, 1] == bilTahminDeger[1, 1])//eksilerde değişme yoksa
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (bilTahminX[i, 2] == -1  )
+                        {
+                            if (j == -1 && bilTahminX[i, 1] == -1) j = i;
+                            else if (j != -1 && bilTahminX[i, 1] != -1)//eğer sabit hafızada yeterli yer yoksa buraya girmez
+                            {
+                                bilTahminX[j, 1] = bilTahminX[i, 1];
+                                bilTahminX[i, 1] = bilTahminX[j, 0];
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            bilTahminDeger[1, 0] = bilTahminDeger[0, 0];//artiları hafızaya al
+            bilTahminDeger[1, 1] = bilTahminDeger[0, 1];//eksileri hafıyaza al
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (bilTahminX[i, 2] == -1 && bilTahminX[i, 1] == -1)
+                {
+                    if (i == 0) sonTah[i] = new Random().Next(1, 10);
+                    else sonTah[i] = new Random().Next(0, 10);
+                }
+                else if (bilTahminX[i, 1] != -1) sonTah[i] = bilTahminX[i, 1];
+                else sonTah[i] = bilTahminX[i, 0];
+
+            }
+            bilSonTahmin = sayila(sonTah);
+
+        }
+
+
+        private int rastgeleTahmin()
+        {
+           return  new Random().Next(1000, 10000);
+        }
+        private void bilTahimB_Click(object sender, EventArgs e)
+        {
+            this.siraKontrol(2);
+            // ilk rastgele tahmin
+            if (!ilkTahmin)
+            {
+                ilkTahmin = true;
+                bilSonTahmin = this.rastgeleTahmin();
+            }
+            else
+            {
+                //tahmin ip ucu yoksa yeni bir rastgele tahmin yap
+                if (bilTahminDeger[0, 0] == 0 && bilTahminDeger[0, 1] == 0)
+                {
+                    bilSonTahmin = this.rastgeleTahmin();
+                }
+                    // else tahmin işlemi yap
+                else this.bilTahmin();
+            }
+            bilTahminT.Text = bilSonTahmin + "";
+        }
+
+        // Başla - kullanıcı değerlendime sonuc text kontrol
+        private string textKotrol(string t)
+        {
+            if (!string.IsNullOrEmpty(t))
+            {
+                int s;
+                if (!int.TryParse(t, out s)) MessageBox.Show("Lütfen rakam giriniz!");
+                if (s > 4) return "4";
+                else if (s < 0) return "0";
+                return s.ToString();
+            }
+            return "";
+        }
+
+        private void artiT_TextChanged(object sender, EventArgs e)
+        {
+            artiT.Text = textKotrol(artiT.Text);
+        }
+
+        private void eksiT_TextChanged(object sender, EventArgs e)
+        {
+            eksiT.Text = textKotrol(eksiT.Text);
+        }
+
+        // Bitiş - kullanıcı değerlendime sonuc text kontrol
+
+
+        private void degerB_Click(object sender, EventArgs e)
+        {
+            siraKontrol(0);
+            if (int.Parse(artiT.Text) == 4)
+            {
+                if (DialogResult.Yes
+                    == MessageBox.Show(
+                        "Bilgisayar oyunu kazandı. Oyun Yeniden Başlasın mı?",
+                        "Uyarı!!",
+                        MessageBoxButtons.YesNo)) Application.Restart();
+            }
+
+            else
+            {
+                if (string.IsNullOrEmpty(artiT.Text)) bilTahminDeger[0, 0] = 0;
+                else bilTahminDeger[0, 0] = int.Parse(artiT.Text);
+                if (string.IsNullOrEmpty(eksiT.Text)) bilTahminDeger[0, 1] = 0;
+                else bilTahminDeger[0, 1] = int.Parse(eksiT.Text);
+            }
+        }
+
+        private void artiT_Enter(object sender, EventArgs e)
+        {
+            artiT.Text = "";
+        }
+
+        private void eksiT_Enter(object sender, EventArgs e)
+        {
+            eksiT.Text = "";
+        }
+    }
+}
